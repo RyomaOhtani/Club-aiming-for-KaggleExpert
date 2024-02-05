c8e70fcff8144903bd40f9caf0fdcb046a447d1d
RyomaOhtani <jukenohtani@gmail.com>
2024-02-05 21:42:11 +0900

    変更

--- Ryoma/ryoma001.py
+++ Ryoma/ryoma001.py
@@ -1,3 +1,12 @@
 #%%
-print('Hello World!')
+import seaborn as sns
+#%%
+# データの準備
+tips = sns.load_dataset("tips")
+
+# グラフの作成
+sns.barplot(x="day", y="total_bill", data=tips)
+
+# グラフの表示
+plt.show()
 # %%
